42216f63ba58fbbc080e5f7016ac01e7a8e1abde
Prashant Jalan <pj.prashantjalan@gmail.com>
2014-03-15 12:27:02 +0530

    The new grammar is bullshit

--- grammar.py
+++ grammar.py
@@ -5,6 +5,7 @@ token2=['{', '(', '}' , ')', '[', ']', '.', ',', '%', '&', '!', '+', '-', '*', '
 tokenrep = ['IDENTIFIER', 'CONSTANT', 'STRING' 'SIZEOF', 'PTR_OP', 'INC_OP', 'DEC_OP', 'LEFT_OP', 'RIGHT_OP', 'LE_OP', 'GE_OP', 'EQ_OP', 'NE_OP', 'AND_OP', 'OR_OP', 'MUL_ASSIGN', 'DIV_ASSIGN', 'MOD_ASSIGN', 'ADD_ASSIGN', 'SUB_ASSIGN', 'LEFT_ASSIGN', 'RIGHT_ASSIGN', 'AND_ASSIGN', 'XOR_ASSIGN', 'OR_ASSIGN', 'TYPEDEF', 'EXTERN', 'STATIC', 'AUTO', 'REGISTER', 'CHAR', 'SHORT', 'INT', 'LONG', 'SIGNED', 'UNSIGNED', 'FLOAT', 'DOUBLE', 'CONST', 'VOLATILE', 'VOID', 'STRUCT', 'UNION', 'ENUM', 'ELLIPSIS', 'CASE', 'DEFAULT', 'IF', 'ELSE', 'SWITCH', 'WHILE', 'DO', 'FOR', 'GOTO', 'CONTINUE', 'BREAK', 'RETURN']
 token2rep=['LEFT_CURL','LEFT_ROUND','RIGHT_CURL','RIGHT_ROUND','LEFT_SQUARE','RIGHT_SQUARE','DOT','COMMA','MODULO','AMPERSAND','EXCLAMATION','PLUS','MINUS','MULTIPLY','DIVIDE','TILDA','HASH','L_OP','G_OP','LE_OP','EQUAL','GE_OP','ADD_ASSIGN','SUB_ASSIGN','POWER','PIPE','QUESTION','COLON','SEMICOLON']
 #for line in f.readline():
+
 lines = f.readlines();
 k=-1;
 while k < len(lines):
@@ -27,7 +28,7 @@ while k < len(lines):
     for j in range(0,len(list1)):
         if(list1[j][1:-1] in token2):
             index= token2.index(list1[j][1:-1])
-            func = func+ token2rep[index]+' ' 
+            func = func + token2rep[index] + ' ' 
         else:
             func=func+ list1[j]+' '
     func= func[:-1] +'\'\n\tt[0]=Node(\''+x+'\', ['
